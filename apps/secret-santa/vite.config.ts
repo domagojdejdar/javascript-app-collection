427a9b129b336fedc6460a51c2a5202bf500cefe
Domagoj Dejdar <domagoj@conway.expert>
2025-12-26 12:37:17 +0100

    Fix some routes

--- apps/secret-santa/vite.config.ts
+++ apps/secret-santa/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: mode === 'production' ? '/secret-santa/' : '/',
+    base: mode === 'production' ? '/javascript-app-collection/secret-santa/' : '/',
     resolve: {
       alias: {
         '@': resolve(__dirname, './src'),
